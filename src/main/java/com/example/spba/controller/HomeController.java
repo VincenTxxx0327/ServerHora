f383b132d63d87cf433c319b75b77ce4d8e0dd86
VincenT <vincentli.tech@gmail.com>
2026-04-15 13:35:57 +0800

    修复外链访问兼容后缀问题

---
+++ src/main/java/com/example/spba/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.example.spba.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/spba-api/")
+    public void redirectSpbaRoot(HttpServletResponse response) throws IOException {
+        // 302 重定向到 /spba-api/home/index.html
+        System.out.println("=======>>redirectSpbaRoot");
+        response.sendRedirect("/spba-api/home/index.html");
+    }
+
+}
